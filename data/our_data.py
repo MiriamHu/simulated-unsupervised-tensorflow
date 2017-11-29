8f83b96a4d69b583e772bdeb7d5237c6022c5f00
Miriam <huijsermiriam@gmail.com>
2017-11-29 13:51:24 +0100

    no real dim

--- data/our_data.py
+++ data/our_data.py
@@ -37,13 +37,13 @@ class DataLoader(object):
 
         self.synthetic_data_paths.sort()
 
-        if np.rank(self.real_data) == 3:
-            self.real_data = np.expand_dims(self.real_data, -1)
+        # if np.rank(self.real_data) == 3:
+        #     self.real_data = np.expand_dims(self.real_data, -1)
 
         self.real_p = 0
 
-    def get_observation_size(self):
-        return self.real_data.shape[1:]
+    # def get_observation_size(self):
+    #     return self.real_data.shape[1:]
 
     def get_num_labels(self):
         return np.amax(self.labels) + 1
